149c9071120197be050948b237898bd1d4dcaa7d
Abhimanyu Patil <abhipatil9798@gmail.com>
2024-08-11 19:00:10 +0530

    ems dao and service layer completed also written exceptions and dto's for furthur process

---
+++ src/main/java/com/app/dao/ProjectDao.java
@@ -0,0 +1,10 @@
+package com.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.entities.Address;
+import com.app.entities.Project;
+
+public interface ProjectDao extends JpaRepository<Project, Long>{
+
+}
---
+++ src/main/java/com/app/dao/ProjectEmployeeDetailsDao.java
@@ -0,0 +1,9 @@
+package com.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.entities.Address;
+
+public interface ProjectEmployeeDetailsDao extends JpaRepository<ProjectEmployeeDetailsDao, Long>{
+
+}
